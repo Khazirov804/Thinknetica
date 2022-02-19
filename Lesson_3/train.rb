6e38da1a1b5d3bf0613ea4c675974d24975d52e9
Khazirov804 <90681581+Khazirov804@users.noreply.github.com>
2022-02-19 18:58:43 +0300

    Add files via upload

---
+++ Lesson_3/train.rb
@@ -0,0 +1,64 @@
+class Train
+  attr_reader :carriages, :station, :number, :type, :speed, :route, :current_station
+  def initialize(number, type, carriages)
+    @number = number
+    @type = type
+    @carriages = carriages
+    @speed = 0
+  end
+
+  def set_route(route)
+    @route = route
+    @current_station = route.stations.first
+    current_station.add_train(self)
+  end
+
+  def speed_up(velocity) #набирает скорость
+    @speed += velocity
+  end
+
+  def slow_down #сбрасывает скорость
+    @speed = 0
+  end
+
+  def number_of_cars #возвращает количество вагонов
+    @carriages = carriages
+  end
+
+  def add_carriages
+    @carriages += 1 if @speed == 0
+  end
+
+  def delete_carriages
+    @carriages -= 1 if speed == 0 && carriages > 0
+  end
+
+  def forward
+    if next_station
+      current_station.delete_train(self)
+      @current_station = next_station
+      current_station.add_train(self)
+    end
+  end
+
+  def backward
+    if prev_station
+      current_station.delete_train(self)
+      @current_station = prev_station
+      current_station.add_train(self)
+    end
+  end
+
+  def current_station_index
+    @route.stations.index(@current_station)
+  end
+
+  def next_station
+    @route.stations[current_station_index + 1]
+  end
+
+  def prev_station
+    return if current_station_index.zero?
+    @route.stations[current_station_index - 1]
+  end
+end
