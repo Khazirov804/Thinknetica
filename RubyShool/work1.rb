7762ce78ae16b654ff3f6d55990fbac51c6e653d
Khazirov804 <90681581+Khazirov804@users.noreply.github.com>
2022-12-06 18:22:48 +0300

    Add files via upload

---
+++ RubyShool/work1.rb
@@ -0,0 +1,20 @@
+Salary = 3000
+
+puts "Salary"
+puts Salary
+
+puts "Tax"
+puts 0.40
+
+puts "Final"
+puts Salary - Salary * 0.40
+
+puts "Annual Salary"
+puts Salary * 12
+
+puts "Enter your name"
+myname = gets
+puts myname
+
+puts "Program finished. Press Enter to Exit"
+gets
